1d6c39188b233573da12467c4fc08916fd161819
ericrochat99-sys <ericrochat99@gmail.com>
2026-09-04 07:55:23 -0400

    Rebrand SketchUp extension as ForgeCase

--- skilled_services.rb
+++ skilled_services.rb
@@ -7,13 +7,13 @@ require_relative "skilled_services/version"
 module SkilledServices
   unless file_loaded?(__FILE__)
     extension = SketchupExtension.new(
-      "Skilled Services – Euro Cabinet Generator",
+      "ForgeCase",
       File.join(__dir__, "skilled_services", "main")
     )
     extension.description = "Create configurable European-style cabinets, shop-drawing scenes, and cabinet reports."
     extension.version = VERSION
-    extension.creator = "Skilled Services"
-    extension.copyright = "Copyright #{Time.now.year} Skilled Services"
+    extension.creator = "ForgeCase"
+    extension.copyright = "Copyright #{Time.now.year} ForgeCase"
 
     Sketchup.register_extension(extension, true)
     file_loaded(__FILE__)
